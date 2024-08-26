b99d42d64e890c133873a0c4d6f2e6ec0fc167bb
athul0rameshan9 <111219465+athul0rameshan9@users.noreply.github.com>
2024-08-26 17:59:05 +0530

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+
+# Initialize the Chrome WebDriver
+driver = webdriver.Chrome()
+
+# Open the login page
+driver.get('http://172.16.16.1:1000/login?5723b4f0c234a97f')
+
+# Locate the username and password fields
+username_field = driver.find_element(By.NAME, "username")
+password_field = driver.find_element(By.NAME, "password")
+
+#the loop starts here
+name = "athul_b210880ee"#place your username here
+password = "b210880ee"#place your password here
+
+
+
+
+
+# Enter your login credentials
+username_field.send_keys(name)
+password_field.send_keys(password)
+
+# Submit the form
+password_field.send_keys(Keys.RETURN)
+
+# Wait for the login process to complete
+time.sleep(2)
+
+while True:
+    time.sleep(1)
+
+
+
+#code to logout
+"""
+try:
+    time.sleep(2) 
+    logout_link = driver.find_element(By.LINK_TEXT, "logout")
+    logout_link.click()
+    time.sleep(2)  # Wait for the logout process to complete
+    print("Logout successful!")
+except Exception as e:
+    print("Logout failed:", e)
+    driver.back()
+"""
+
+# Close the browser
+
